25d70eb52ccd31d4183e0ce02307b328fb422b58
adib-commits <hdadib442@gmail.com>
2026-04-10 20:52:23 +0000

    that sorts an array of integers in ascending order using the Selection sort algorithm

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,34 +1,34 @@
 #include "sort.h"
 
 /**
- * selection_sort - Sorts an array using selection sort algorithm
- * @array: Pointer to the array
- * @size: Number of elements in the array
+ * selection_sort - Sorts an array of integers in ascending order
+ * using the Selection sort algorithm
+ *
+ * @array: The array to be sorted
+ * @size: Number of elements in @array
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, min_index;
-    int temp;
+	size_t i, j, min_idx;
+	int tmp;
 
-    if (array == NULL || size < 2)
-        return;
+	if (!array || size < 2)
+		return;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        min_index = i;
-
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[min_index])
-                min_index = j;
-        }
-
-        if (min_index != i)
-        {
-            temp = array[i];
-            array[i] = array[min_index];
-            array[min_index] = temp;
-            print_array(array, size);
-        }
-    }
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+				min_idx = j;
+		}
+		if (min_idx != i)
+		{
+			tmp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = tmp;
+			print_array(array, size);
+		}
+	}
 }
