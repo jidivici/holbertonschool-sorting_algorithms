513d962767c74ecd3bed1144e4392d494ed1fe23
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-04-10 18:14:11 +0200

    add: function sort by insertion print a linked list sorted

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,27 +10,27 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *tmp, *curr, *prev;
 
-        if (!list || !*list)
-        	return;
-        curr = (*list)->next;
-        while (curr)
-        {
-        	tmp = curr->next;
-              	while (curr->prev && curr->n < curr->prev->n)
+	if (!list || !*list)
+		return;
+	curr = (*list)->next;
+	while (curr)
+	{
+		tmp = curr->next;
+		while (curr->prev && curr->n < curr->prev->n)
 		{
-                	prev = curr->prev;
-                	if (prev->prev)
-                		prev->prev->next = curr;
-                        curr->prev = prev->prev;
-                        prev->next = curr->next;
-                        if (curr->next)
-                        	curr->next->prev = prev;
-                        curr->next = prev;
-                        prev->prev = curr;
-                        if (!curr->prev)
-                        	*list = curr;
-                        print_list(*list);
-                }
+			prev = curr->prev;
+			if (prev->prev)
+				prev->prev->next = curr;
+			curr->prev = prev->prev;
+			prev->next = curr->next;
+			if (curr->next)
+				curr->next->prev = prev;
+			curr->next = prev;
+			prev->prev = curr;
+			if (!curr->prev)
+				*list = curr;
+			print_list(*list);
+		}
 	curr = tmp;
-        }
+	}
 }
